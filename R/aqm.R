16afca86480f083f294b2c5b79355912d385b9b1
dgolicher <dgolicher@gmail.com>
2019-07-19 14:34:03 +0000

    Initial commit

--- R/aqm.R
+++ R/aqm.R
@@ -4,3 +4,23 @@ dt<-function(x) DT::datatable(x,
                                 dom = 'Blfrtip',
                                 buttons = c('copy', 'csv', 'excel'), colReorder = TRUE
                               ))
+
+clean <-function(x)as.numeric(gsub("[^0-9.-]","",as.character(x)))
+
+f_met<-function(nm="hurn",skip=7)
+{
+  URL<-sprintf("https://www.metoffice.gov.uk/pub/data/weather/uk/climate/stationdata/%sdata.txt",nm) 
+  d<-read.table(URL,skip=skip, skipNul = TRUE, fill=TRUE,flush=TRUE)
+  names(d)<- c("Year", "Month", "tmax", "tmin",  "af","rain", "sun") 
+  d$Year<-clean(d$Year)
+  d$tmin<-clean(d$tmin)
+  d$tmax<-clean(d$tmax)
+  d$af<-clean(d$af)
+  d$rain<-clean(d$rain)
+  d$sun<-clean(d$sun)
+  d$date<-as.Date( paste(d$Year,d$Month , 15 , sep = "/" )  , format = "%Y/%m/%d" )
+  d$month<-lubridate:::month(d$date,label=TRUE)
+  d$station<-nm
+  d
+}
+
